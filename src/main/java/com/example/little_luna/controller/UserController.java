e31b028f59f13f87a612c7fd51bfc59426e81b3c
lyanbrito <lyanbrito613@gmail.com>
2025-09-25 08:16:19 -0300

    fix: added dependences

--- src/main/java/com/example/little_luna/controller/UserController.java
+++ src/main/java/com/example/little_luna/controller/UserController.java
@@ -5,13 +5,11 @@ import com.example.little_luna.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-
 public class UserController {
     @Autowired
     UserRepo userr;
